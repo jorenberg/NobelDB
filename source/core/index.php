726504b6812326b9bf54d10f7ce9bdb38eea0a6f
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-22 02:18:01 +0530

    Initial commit
    The HomePage of NobelDB®.

---
+++ source/core/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The HomePage of NobelDB.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package NobelDB - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
