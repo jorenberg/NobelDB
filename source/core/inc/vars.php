3aa39eac30d3674e774d1c8f8c1677fa9344e620
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-22 02:44:10 +0530

    Initial commit
    Creates common globals for the NobelDB®.

---
+++ source/core/inc/vars.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Creates common globals for the rest of NobelDB.
+ *
+ * Simple browser detection.
+ * -- mobile devices (all Android, BlackBerry, iPhone, iPad, etc.)
+ * -- all tablets.
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package NobelDB - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
