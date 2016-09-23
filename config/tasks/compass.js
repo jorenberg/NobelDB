fa52c7169e3b602bfa564bbb1d5919a2c862e484
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-24 00:06:46 +0530

    Initial commit
    Path: config/tasks/compass.js
    Usage: $ npm install grunt-contrib-compass --save-dev

---
+++ config/tasks/compass.js
@@ -0,0 +1,24 @@
+/*!
+ * NobelDB®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~~~
+ * grunt-contrib-compass : https://github.com/gruntjs/grunt-contrib-compass
+ * Usage                 : $ npm install grunt-contrib-compass --save-dev
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ________________________________________________________________________
+ * @date      : 24-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ________________________________________________________________________
+ * Primarily Designed for WebKit Engine(s): -------------
+ * Safari — Version 10.0 (11602.1.50.0.10).
+ * Copyright © 2003 – 2016 Apple Inc. All rights reserved.
+ * ________________________________________________________________________
+ */
