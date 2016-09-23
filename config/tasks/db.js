602c5195383f2facb83133ea7480f7dba1cf612e
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-24 00:56:10 +0530

    Initial commit
    Path: config/tasks/db.js
    MySQL: A Pure Node.js JavaScript Client Implementing the MySQL Protocol.
    Usage: $ npm install mysql

---
+++ config/tasks/db.js
@@ -0,0 +1,22 @@
+/*!
+ * NobelDB®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~
+ * MySQL      : https://github.com/mysqljs/mysql
+ * A Pure Node.js JavaScript Client Implementing the MySQL Protocol.
+ * Usage      : $ npm install mysql
+ * @require   : MySQL RDBMS - http://www.mysql.com/.
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ________________________________________________________________________
+ * @date      : 24-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ________________________________________________________________________
+ */
