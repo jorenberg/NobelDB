866694bdb81596b631c59d3307333b66e4da0b2d
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-28 06:36:50 +0530

    Initial commit
    Using require in R.
    Help: ggplot2 | http://ggplot2.org/.

--- Analysis/analysis.R
+++ Analysis/analysis.R
@@ -14,3 +14,11 @@
 
 # Remove Objects from a Specified Environment.
 rm(list = ls())
+
+# ggplot2
+# ggplot2 is a plotting system for R, based on the grammar of graphics.
+if (!require("ggplot2")) {
+  stop('The package ggplot2 was not installed!')
+  } else {
+  install.packages("ggplot2")
+}
